49179cdf3ff91e3c364de9ee810270e18e736890
Grootcode21 <antonyndichu15@gmail.com>
2024-10-17 15:31:30 -0700

    quiz game questions

--- quiz_game.py
+++ quiz_game.py
@@ -25,3 +25,20 @@ if response == "graphics processing unit":
 else:
     print("Oops, incorrect!")
 
+response = input("What does OTG stand for?")
+if response == "on the go":
+    print("Correct!")
+else:
+    print("Oops, incorrect!")
+
+response = input("What is an ATM?")
+if response == "automated teller machine":
+    print("Correct!")
+else:
+    print("Oops, incorrect!")
+
+response = input("What does PDF stand for?")
+if response == "portable document format":
+    print("Correct!")
+else:
+    print("Oops, incorrect!")
