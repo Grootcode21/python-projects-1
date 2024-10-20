c707713d1c6f5f279fd2ecdc7e8b5cd1d921c245
Grootcode21 <antonyndichu15@gmail.com>
2024-10-20 23:24:19 +0300

    counting scores

--- quiz_game.py
+++ quiz_game.py
@@ -6,52 +6,61 @@ if playing != "Yes":
     quit()
 
 print("Great, Lets play the game")
+score = 0
 
 response = input("What does OS stand for?")
 if response == "operating system":
     print("Correct!")
+    score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("Which OS are you using?")
 if response == "windows 11":
     print("Correct!")
+    score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does GPU stand for?")
 if response == "graphics processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does OTG stand for?")
 if response == "on the go":
     print("Correct!")
+    score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What is an ATM?")
 if response == "automated teller machine":
     print("Correct!")
+    score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does PDF stand for?")
 if response == "portable document format":
     print("Correct!")
+    score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does GPS stand for?")
 if response == "global positioning system":
     print("Correct!")
+    score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does HDMI stand for?")
 if response == "high definition multimedia interface":
     print("Correct!")
+    score += 1
 else:
     print("Oops, incorrect!")
 
