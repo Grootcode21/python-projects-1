5d853c6ddbf86bc5439e0afdc84148d6c5f1ac1a
Grootcode21 <antonyndichu15@gmail.com>
2024-10-21 18:59:52 +0300

    counting scores

--- quiz_game.py
+++ quiz_game.py
@@ -9,143 +9,144 @@ print("Great, Lets play the game")
 score = 0
 
 response = input("What does OS stand for?")
-if response == "operating system":
+if response.lower() == "operating system":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("Which OS are you using?")
-if response == "windows 11":
+if response.lower() == "windows 11":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does GPU stand for?")
-if response == "graphics processing unit":
+if response.lower() == "graphics processing unit":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does OTG stand for?")
-if response == "on the go":
+if response.lower() == "on the go":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What is an ATM?")
-if response == "automated teller machine":
+if response.lower() == "automated teller machine":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does PDF stand for?")
-if response == "portable document format":
+if response.lower() == "portable document format":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does GPS stand for?")
-if response == "global positioning system":
+if response.lower() == "global positioning system":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does HDMI stand for?")
-if response == "high definition multimedia interface":
+if response.lower() == "high definition multimedia interface":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does LCD stand for?")
-if response == "liquid crystal display":
+if response.lower() == "liquid crystal display":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does LED stand for?")
-if response == "light emmiting diode":
+if response.lower() == "light emmiting diode":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What is Wi-Fi?")
-if response == "wireless fidelity":
+if response.lower() == "wireless fidelity":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does GPRS stand for?")
-if response == "general packet radio service":
+if response.lower() == "general packet radio service":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does DVD stand for?")
-if response == "digital versatile disc":
+if response.lower() == "digital versatile disc":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What is the brain of the computer?")
-if response == "central processing unit":
+if response.lower() == "central processing unit":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does RAM stand for in computing?")
-if response == "random accesss memory":
+if response.lower() == "random accesss memory":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("Which operating system is developed by Microsoft?")
-if response == "windows":
+if response.lower() == "windows":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does HTTP stand for?")
-if response == "hypertext transfer protocol":
+if response.lower() == "hypertext transfer protocol":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does GUI stand for?")
-if response == "graphical user interface":
+if response.lower() == "graphical user interface":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does BIOS stand for?")
-if response == "basic input output system":
+if response.lower() == "basic input output system":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
 response = input("What does SSD stand for?")
-if response == "solid state drive":
+if response.lower() == "solid state drive":
     print("Correct!")
     score += 1
 else:
     print("Oops, incorrect!")
 
-print("Your final score is:", score, "/ 30")
+print("Your final score is:", str(score), "/ 20")
+
