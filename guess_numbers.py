abf2838bbc0b48cb155ac1e61281579265ad4c42
Grootcode21 <antonyndichu15@gmail.com>
2024-10-25 23:03:38 +0300

    user input

--- guess_numbers.py
+++ guess_numbers.py
@@ -5,4 +5,9 @@ top_of_range = input("Please type a number: ")
 if not top_of_range.isdigit():
     print("Error: You must enter a number.")
 
- 
+while True:
+    try:
+        number = random.randint(1, int(top_of_range))
+        break
+    except ValueError:
+        print("Error: You must enter a valid number.")
