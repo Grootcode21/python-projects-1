0a4db064e2b9236972c9ae0106925bb4c83eb747
Grootcode21 <antonyndichu15@gmail.com>
2024-10-28 14:05:13 +0300

    user input

--- guess_numbers.py
+++ guess_numbers.py
@@ -2,7 +2,7 @@ import random
 
 top_of_range = input("Please type a number: ")
 
-if not top_of_range.isdigit():
+if top_of_range.isdigit():
     top_of_range = int(top_of_range)
 
     if top_of_range <= 0:
@@ -14,8 +14,10 @@ else:
     quit()
 
 random_no = random.randint(0, top_of_range)
-print("The random number is:", random_no)
 
+while True:
+    guess = int(input(f"Guess a number between 0 and {top_of_range}: "))
+     
 
 
 
