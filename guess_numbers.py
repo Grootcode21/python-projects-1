f9b23729c76bc6747a3d96d5fae82630135d73f7
Grootcode21 <antonyndichu15@gmail.com>
2024-10-28 15:53:45 +0300

    user input

--- guess_numbers.py
+++ guess_numbers.py
@@ -16,7 +16,16 @@ else:
 random_no = random.randint(0, top_of_range)
 
 while True:
-    guess = int(input(f"Guess a number between 0 and {top_of_range}: "))
+    user_guess = input(f"Guess a number between 0 and {top_of_range}: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+        if user_guess == random_no:
+            print("Congratulations! You guessed the correct number.")
+            break
+        elif user_guess < random_no:
+            print("Too low! Try again.")
+        else:
+            print("Too high! Try again.")
      
 
 
