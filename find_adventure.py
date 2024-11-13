3b362d2e88643024c06f05940421d9637ed863a3
Grootcode21 <antonyndichu15@gmail.com>
2024-11-13 15:57:42 +0300

    initial commit

--- find_adventure.py
+++ find_adventure.py
@@ -1,2 +1,4 @@
 name = input("Please enter you name: ")
 print("Hello",name, "Welcome to my adventure game!")
+
+answer = input("You have come to an end of a dirt road. Which way would you like to go?")
