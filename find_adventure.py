8ab14f3552ff92d33ac7c9398cfa23e13ed4fcaa
Grootcode21 <antonyndichu15@gmail.com>
2024-11-14 16:17:14 +0300

    commits

--- find_adventure.py
+++ find_adventure.py
@@ -1,4 +1,12 @@
 name = input("Please enter you name: ")
 print("Hello",name, "Welcome to my adventure game!")
 
-answer = input("You have come to an end of a dirt road. Which way would you like to go?")
+answer = input("You have come to an end of a dirt road. Would like to go left or right?")
+
+if answer == "left":
+    
+    elif answer == "right":
+
+else:
+
+    
