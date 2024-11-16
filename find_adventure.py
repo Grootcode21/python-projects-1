5f8c0291f9a1122f2a1f4a45dd41223b165191a5
Grootcode21 <antonyndichu15@gmail.com>
2024-11-16 22:38:31 +0300

    commits

--- find_adventure.py
+++ find_adventure.py
@@ -4,9 +4,12 @@ print("Hello",name, "Welcome to my adventure game!")
 answer = input("You have come to an end of a dirt road. Would like to go left or right?")
 
 if answer == "left":
+    answer == "you come to a river. You can walk around it or swim accross it."
     
-    elif answer == "right":
+elif answer == "right":
+    print()
 
 else:
+    print('This is not a valid answer. You lose')
 
     
