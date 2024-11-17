0b14b5f2fa3aafc2c2bb3e3cefb45f50b7c95ac8
Grootcode21 <antonyndichu15@gmail.com>
2024-11-17 11:51:08 +0300

    commits

--- find_adventure.py
+++ find_adventure.py
@@ -5,6 +5,15 @@ answer = input("You have come to an end of a dirt road. Would like to go left or
 
 if answer == "left":
     answer == "you come to a river. You can walk around it or swim accross it."
+
+    if answer == "walk around":
+        answer = "you find a hidden cave. You can enter or leave."
+        
+    elif answer == "swim across":
+        answer = "you drown and die."
+
+    else:
+        print('This is not a valid option')
     
 elif answer == "right":
     print()
