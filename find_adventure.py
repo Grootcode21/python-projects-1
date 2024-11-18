a1f6642bd02107cd9b2dcacf0d4786d1b229abfa
Grootcode21 <antonyndichu15@gmail.com>
2024-11-18 14:49:05 +0300

    commits

--- find_adventure.py
+++ find_adventure.py
@@ -1,15 +1,15 @@
 name = input("Please enter you name: ")
 print("Hello",name, "Welcome to my adventure game!")
 
-answer = input("You have come to an end of a dirt road. Would like to go left or right?")
+answer = input("You have come to an end of a dirt road. Would like to go left or right?").lower()
 
 if answer == "left":
-    answer == "you come to a river. You can walk around it or swim accross it."
+    answer == input("you come to a river. You can walk around it or swim accross it.")
 
-    if answer == "walk around":
+    if answer == "walk":
         answer = "you find a hidden cave. You can enter or leave."
         
-    elif answer == "swim across":
+    elif answer == "swim":
         answer = "you drown and die."
 
     else:
