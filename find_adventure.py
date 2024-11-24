559b4c4f1a387b58334e5f0e0844df6c349d2501
Grootcode21 <antonyndichu15@gmail.com>
2024-11-24 17:59:46 +0300

    nested if statements

--- find_adventure.py
+++ find_adventure.py
@@ -4,7 +4,7 @@ print("Hello",name, "Welcome to my adventure game!")
 answer = input("You have come to an end of a dirt road. Would like to go left or right?").lower()
 
 if answer == "left":
-    answer == input("you come to a river. You can walk around it or swim accross it.")
+    answer == input("you come to a river. You can walk around it or swim accross it.").lower()
 
     if answer == "walk":
         answer = "you find a hidden cave. You can enter or leave."
@@ -13,7 +13,7 @@ if answer == "left":
             answer = "you get bitten by a venomous snake and you lose the game."
         
         elif answer == "leave":
-            answer = "you come across a lonely pathway.You can go foward or go back"
+            answer = "you go back to the main route"
 
         else:
             print('This is not a valid option')
@@ -25,13 +25,22 @@ if answer == "left":
         print('This is not a valid option')
     
 elif answer == "right":
-    answer == input("you come to a bridge which looks wobbly. You can cross or go back(cross/back).")
+    answer == input("you come to a bridge which looks wobbly. You can cross or go back(cross/back).").lower()
 
     if answer == "back":
         answer = "you go back and you lose the game."
         
     elif answer == "cross":
-        answer = "you come across a stranger. Do you talk to them"
+        answer = "you come across a stranger. Do you talk to them?(yes/no)"
+
+        if answer == "yes":
+            answer = "They give you gold.You win!"
+        
+        elif answer == "no":
+            answer = "you ignore them. You lose the game"
+
+        else:
+            print('This is not a valid option')
 
     else:
         print('This is not a valid option')
@@ -39,5 +48,7 @@ elif answer == "right":
 
 else:
     print('This is not a valid answer. You lose')
+
+print("Thank you trying", name)
 
     
