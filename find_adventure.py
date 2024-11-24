1899e36c91183b94dee4ba81f756d54c04c95d2c
Grootcode21 <antonyndichu15@gmail.com>
2024-11-24 17:49:37 +0300

    nested if statements

--- find_adventure.py
+++ find_adventure.py
@@ -25,7 +25,16 @@ if answer == "left":
         print('This is not a valid option')
     
 elif answer == "right":
-    answer == input("you come to an end. You can go left or quit.")
+    answer == input("you come to a bridge which looks wobbly. You can cross or go back(cross/back).")
+
+    if answer == "back":
+        answer = "you go back and you lose the game."
+        
+    elif answer == "cross":
+        answer = "you come across a stranger. Do you talk to them"
+
+    else:
+        print('This is not a valid option')
 
 
 else:
