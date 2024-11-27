67d1b37750a92ad81713542289601e9da2557532
Grootcode21 <antonyndichu15@gmail.com>
2024-11-27 22:01:26 +0300

    while loop

--- password_manager.py
+++ password_manager.py
@@ -2,3 +2,20 @@ master_pwd = input("What is the master password?")
 
 def view():
     pass
+
+def add():
+    pass
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones?(add|view)")
+
+    if mode == "q":
+        break
+
+    if mode == "add":
+        pass
+    if mode == "view":
+        pass
+    else:
+        print("Invalid mode.")
+        continue
