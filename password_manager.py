a6900e215d5cdaf212f45270102db5bf4e73476a
Grootcode21 <antonyndichu15@gmail.com>
2024-12-12 22:16:42 +0300

    loading the key

--- password_manager.py
+++ password_manager.py
@@ -8,7 +8,13 @@ def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
         key_file.write(key)'''
-        
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
 master_pwd = input("What is the master password?")
 
 
