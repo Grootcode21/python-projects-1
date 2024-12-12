9ff9c0f753752113ef19668b133d09fcf8ba33dc
Grootcode21 <antonyndichu15@gmail.com>
2024-12-12 22:15:11 +0300

    writing the key

--- password_manager.py
+++ password_manager.py
@@ -2,7 +2,16 @@
 from cryptography.fernet import  Fernet
 
 
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+        
 master_pwd = input("What is the master password?")
+
+
 
 def view():
     with open('password.txt', 'r') as f:
