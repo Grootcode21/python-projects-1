9ccd6b76cba37721b01ecd6dfec5f59168e173e8
Grootcode21 <antonyndichu15@gmail.com>
2024-12-15 00:08:28 +0300

    write key function

--- password_manager.py
+++ password_manager.py
@@ -2,12 +2,10 @@
 from cryptography.fernet import  Fernet
 
 
-
-'''
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)'''
+        key_file.write(key)
 
 def load_key():
     file = open("key.key", "rb")
