cd2ccb7c5b41929ba5d9f203d6ee652aed6cc1cf
Grootcode21 <antonyndichu15@gmail.com>
2025-01-04 23:36:47 +0300

    while loop

--- pig_game.py
+++ pig_game.py
@@ -6,3 +6,8 @@ def roll():
     roll = random.randint(min_value, max_value)
 
     return roll
+
+while True:
+    players = input("Enter the number of players (1-4): ")
+    if players.isdigit():
+    
