6212a226f348f3a3c6b326610a7b2ea1724c221c
Grootcode21 <antonyndichu15@gmail.com>
2025-01-05 19:03:42 +0300

    no. of players

--- pig_game.py
+++ pig_game.py
@@ -10,4 +10,7 @@ def roll():
 while True:
     players = input("Enter the number of players (1-4): ")
     if players.isdigit():
+        players = int(players)
+        if 1 <= players <= 4:
+            break
     
