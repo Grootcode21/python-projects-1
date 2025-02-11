dde9e6c7fbcef0407e848f63ff2aea070abec696
Grootcode21 <antonyndichu15@gmail.com>
2025-02-11 23:08:35 +0300

    searching for words

--- madlibs_generator.py
+++ madlibs_generator.py
@@ -1,4 +1,16 @@
 with open("story.txt", "r") as f:
     story = f.read()
 
-print(story)
+words = []
+start_of_the_word = -1
+
+target_start = "<"
+target_end = ">"
+
+for i, char in enumerate(story):
+    if char == target_start:
+        start_of_the_word = i 
+    if char == target_end and start_of_the_word != -1:
+        word = story[start_of_the_word:i+1]
+        words.append(word)
+        start_of_the_word = -1
