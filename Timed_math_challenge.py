7d6f54ef45c001e71f206aa9842eed2290df89e1
Grootcode21 <antonyndichu15@gmail.com>
2025-02-18 15:29:21 +0300

    generate problems

---
+++ Timed_math_challenge.py
@@ -0,0 +1,16 @@
+import random
+
+OPERATORS = ["+", "-", "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+
+def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + " " + operator + " " + str(right)
+    print(expr)
+    return expr
+
+generate_problem()
