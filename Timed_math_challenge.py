c71532585e93bb3f5d05fc04a9d865c2cadc5d17
Grootcode21 <antonyndichu15@gmail.com>
2025-02-18 16:16:28 +0300

    eval

--- Timed_math_challenge.py
+++ Timed_math_challenge.py
@@ -10,7 +10,8 @@ def generate_problem():
     operator = random.choice(OPERATORS)
 
     expr = str(left) + " " + operator + " " + str(right)
-    print(expr)
-    return expr
+    answer = eval(expr)
+    return expr, answer
 
-generate_problem()
+expr, answer = generate_problem()
+print(expr, answer)
