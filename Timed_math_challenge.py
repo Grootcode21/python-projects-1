20d6cc6a5bb906c7744c46b7d045e8b9f43e76b6
Grootcode21 <antonyndichu15@gmail.com>
2025-02-19 16:50:00 +0300

    Adding timer

--- Timed_math_challenge.py
+++ Timed_math_challenge.py
@@ -4,6 +4,7 @@ import time
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
 
 def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
@@ -15,9 +16,19 @@ def generate_problem():
     return expr, answer
 
 wrong = 0
+input("Press Enter to start!")
+print("********************************")
+
+start_time = time.time()
 
 for i in range(TOTAL_PROBLEMS):
     expr, answer = generate_problem()
     guess = input("Problem #" + str(i +1) + ": " + expr + " = " )
     if guess == str(answer):
         break
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print("---------------------------------------------")
+print("Nice work! You finished in ", total_time, " seconds")
